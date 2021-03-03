44fa8e92a86d2d2f34559d73d2594c45a87cc0b0
Karl <5254025+karwa@users.noreply.github.com>
2021-03-03 23:46:33 +0100

    Better benchmarks. There is still a lot to do, but this at least provides reasonable signal over the major parts of the parser (filtering, scanning, percent encoding/decoding, path simplification, writing).

--- benchmark/Sources/WebURLBenchmark/main.swift
+++ benchmark/Sources/WebURLBenchmark/main.swift
@@ -1,46 +1,34 @@
-import Foundation
-import WebURL
+// Copyright The swift-url Contributors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-// This is a really simple benchmark.
-// Really, it should include more things like IP addresses and such, but
-// it seems like NSURL doesn't really try to parse and re-format those at all... so what's the point?
+import Benchmark
 
-let testURLs = repeatElement(
-  [
-    "http://example.com/some/path/who/cares",
-    "ftp://user:password@example.com:21/some/path/who/cares",
-    "file:///usr/local/bin/swift",
-    "https://example.com/some/path/who/cares?query=present&kettle=on#brewing",
-    "http://chart.apis.google.com/chart?chs=500x500&chma=0,0,100,100&cht=p&chco=FF0000%2CFFFF00%7CFF8000%2C00FF00%7C00FF00%2C0000FF&chd=t%3A122%2C42%2C17%2C10%2C8%2C7%2C7%2C7%2C7%2C6%2C6%2C6%2C6%2C5%2C5&chl=122%7C42%7C17%7C10%7C8%7C7%7C7%7C7%7C7%7C6%7C6%7C6%7C6%7C5%7C5&chdl=android%7Cjava%7Cstack-trace%7Cbroadcastreceiver%7Candroid-ndk%7Cuser-agent%7Candroid-webview%7Cwebview%7Cbackground%7Cmultithreading%7Candroid-source%7Csms%7Cadb%7Csollections%7Cactivity|Chart",
-  ], count: 10
-).joined()
-
-
-func doTest(iterations: Int, _ test: () -> Void) {
-  let start = CFAbsoluteTimeGetCurrent()
-  for _ in 0..<iterations {
-    test()
-  }
-  let end = CFAbsoluteTimeGetCurrent()
-
-  print(
-    """
-    Finished. \(iterations) iterations.
-    Average time: \(((end - start)/Double(iterations)) * 1000) ms
-    """)
+// https://github.com/google/swift-benchmark/issues/69
+@inline(__always)
+internal func blackHole<T>(_ x: T) {
+  @_optimize(none)
+  func assumePointeeIsRead(_ x: UnsafeRawPointer) {}
+  withUnsafePointer(to: x) { assumePointeeIsRead($0) }
 }
 
-sleep(1)
-print("NSURL")
-doTest(iterations: 1_000) {
-  for str in testURLs {
-    _ = URL(string: str)
-  }
-}
-sleep(1)
-print("WebURL")
-doTest(iterations: 1_000) {
-  for str in testURLs {
-    _ = WebURL(str, base: nil)
-  }
-}
+// Benchmark plan:
+// - Non-special versions of SpecialNonFile tests.
+// - Cannot-be-a-base URLs
+// - file: URLs
+// - setters
+
+Benchmark.main([
+  constructor_specialNonFile,
+  urlEncoded_Decoded
+])
